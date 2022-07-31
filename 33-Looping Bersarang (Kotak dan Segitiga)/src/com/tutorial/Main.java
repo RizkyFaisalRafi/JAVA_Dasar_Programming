d58f786e1ae0b9f341f12be1f6eabe966a95d3c0
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2022-07-31 17:14:18 +0700

    Delete 33-Looping Bersarang (Kotak dan Segitiga) directory

--- 33-Looping Bersarang (Kotak dan Segitiga)/src/com/tutorial/Main.java
+++
@@ -1,8 +0,0 @@
-package com.tutorial;
-
-public class Main {
-    // Looping bersarang = ada looping didalam looping
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
